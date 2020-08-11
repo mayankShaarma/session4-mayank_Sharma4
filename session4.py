d700290ac663aabd2d84f91360b23c03bdc44f57
Mayank Sharma <67852592+mayankShaarma@users.noreply.github.com>
2020-08-11 18:29:40 +0530

    Add files via upload

--- session4.py
+++ session4.py
@@ -3,104 +3,93 @@ import decimal
 from decimal import Decimal
 import random
 
-class Qualean(object):
-	def __init__(self, value):
-		if(value in [1,0,-1]):
-			self.num = value
+import random
+import decimal
+from decimal import Decimal
+
+with decimal.localcontext() as ctx:
+	ctx.prec = 10
+	class Qualean:
+		def __init__(self, real):
+			if(not(real==0 or real==1 or real==-1)):
+				raise ValueError("Number can only be 0 or 1 or -1")
 			rand = random.uniform(-1, 1)
-			self.number = round((rand * self.num), 10)
-		else:
-			raise AttributeError('Invalid input')
+			decimal_number = Decimal(str(real))
+			self.number = Decimal(str(round((rand * real), 10)))
 
-	def get_number(self):
-		return self.number
-
-	def __and__(self,other_object):
-		if not bool(self.number):
+		def __and__(self, other):
+			if(not bool(self.number)):
+				return False
+			if(isinstance(other, Qualean)):
+				return bool(self.number*other.number)
 			return False
-		else:
-			if isinstance(other_object,Qualean) and other_object.number:
-				return bool(self.number and other_object.number)
+
+		def __or__(self, other):
+			if(bool(self.number)):
+				return True
+			if(isinstance(other, Qualean)):
+				return bool(self.number+other.number)
+			return False
+
+		def __repr__(self):
+			return 'Qualean({0})'.format(self.number)
+
+		def __str__(self):
+			return 'Qualean: internal number ={0}'.format(self.number)
+
+		def __add__(self, other):
+			if(isinstance(other, Qualean)):
+				return self.number+other.number
+			else:
+				return self.number+Decimal(str(other))
+
+		def __mul__(self, other):
+			if(isinstance(other, Qualean)):
+				return self.number*other.number
+			else:
+				return self.number*Decimal(str(other))
+
+		def __eq__(self, other):
+			if isinstance(other, Qualean):
+				return self.number == other.number
 			else:
 				return False
 
-	def __or__(self,other_object):
-		if self.number:
-			return True
-		else:
-			if isinstance(other_object,Qualean) and other_object.number:
-				return bool(self.number or other_object.number)
+		def get_number(self):
+			return self.number
+
+		def __float__(self):
+			return float(self.number)
+
+		def __ge__(self, other):
+			if(isinstance(other, Qualean)):
+				return self.number>=other.number
 			else:
-				return False
+				return self.number>=Decimal(str(other))
 
-	def __repr__(self):
-		return 'Qualean {0}'.format(self.number)
+		def __gt__(self, other):
+			if(isinstance(other, Qualean)):
+				return self.number>other.number
+			else:
+				return self.number>Decimal(str(other))
 
-	def __str__(self):
-		return 'Qualean: internal number ={0}'.format(self.number)
+		def __le__(self, other):
+			if(isinstance(other, Qualean)):
+				return self.number<=other.number
+			else:
+				return self.number<=Decimal(str(other))
 
-	def __add__(self, num):
-		if(isinstance(num, Qualean)):
-			return self.number+num.number
-		return self.number+Decimal(str(num))
+		def __lt__(self, other):
+			if(isinstance(other, Qualean)):
+				return self.number<other.number
+			else:
+				return self.number<Decimal(str(other))
 
-	def __mul__(self, num):
-		if isinstance(num, Qualean):
-			return self.number * num.num
-		return self.number * Decimal(num)
+		def __invertsign__(self):
+			return self.number*Decimal('-1')
 
-	def __eq__(self, num):
-		if isinstance(num, Qualean):
-			return self.number == num.number
-		return False
+		def __bool__(self):
+			return bool(self.number)
 
-	def __float__(self):
-		return float(self.number)
-
-	def __ge__(self, num):
-		if(isinstance(num, Qualean)):
-			return self.number >= num.number
-		return self.number >= Decimal(str(num))
-
-	def __gt__(self, num):
-		if(isinstance(num, Qualean)):
-			return self.number > num.number
-		return self.number>Decimal(str(num))
-
-	def __le__(self, num):
-		if(isinstance(num, Qualean)):
-			return self.number <= num.number
-		return self.number<=Decimal(str(num))
-
-	def __lt__(self, num):
-		if(isinstance(num, Qualean)):
-			return self.number < num.number
-		return self.number<Decimal(str(num))
-
-	def __invertsign__(self):
-		return self.number*Decimal('-1')
-
-	def __bool__(self):
-		return bool(self.number)
-
-	def __sqrt__(self):
-		if self.number < 0:
-			x = self.number * -1
-			return 1j * float(x.sqrt())
-		else:
+		def __sqrt__(self):
 			return self.number.sqrt()
-
-	# def differentQs(q):
-	# 	return
-
-	# def return_function1(q1, q2):
-	# 	return
-
-	# def return_function2(q1, q2):
-	# 	return
-
-	# def random_number(q1, q2):
-	# 	return
-
-	# def multiple_timesQ_equality(q):
-	# 	return
